841ac903eab21b6055f1c2e897c51aee32faadf8
irinavram <irinavram.gmail.com>
2025-02-12 15:09:55 +0100

    redeployed

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+ 
  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
