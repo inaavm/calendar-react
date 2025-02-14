473bc520d981ebdedc686a846865239befd4ac43
irinavram <irinavram.gmail.com>
2025-02-14 11:46:46 +0100

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,9 @@ import BookListForm from './components/BookListForm';
 import"./App.css";
 
 
-
 function App() {
   return (
     <div className="App">
-        <div class="shape-1"></div>
-        <div class="shape-2"></div>
-        <div class="currentdate">
-        <p>
-      
-          <span className="weekday">
-            {new Date().toLocaleDateString(undefined, { weekday: "long" })}
-          </span>
-          {new Date().toLocaleDateString(undefined, { year: "numeric", month: "long", day: "numeric" })}
-        </p>
-        <div class="wavy-circle"></div>
-        </div>
-
       <br></br>
       <br></br>
     {/* <h1> Your calendar</h1> */}
